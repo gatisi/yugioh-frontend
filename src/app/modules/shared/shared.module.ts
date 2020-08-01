9a8187b3b6e1b4fb9570be8912db3c32334cda4c
Olegs <olegs.katajevs@gmail.com>
2020-08-01 15:10:38 +0300

    app.module.ts SharedModule added

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {FormsModule} from "@angular/forms";
 import {MatInputModule} from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 
@@ -13,10 +14,13 @@ import { MatSliderModule } from '@angular/material/slider';
     CommonModule,
     FormsModule,
     MatInputModule,
-    MatSliderModule
+    MatSliderModule,
+    MatMenuModule
   ],
   exports: [
-    MatSliderModule
+    MatSliderModule,
+    MatInputModule,
+    MatMenuModule
   ]
 })
 export class SharedModule { }
