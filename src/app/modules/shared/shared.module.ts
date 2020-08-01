98582c7c2951d45b44bd71ff7b83e135f26e28b0
Vineta <vineta.gajevska@gmail.com>
2020-08-01 14:05:32 +0300

    added shared module and slider in user

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatSliderModule
+  ]
+})
+export class SharedModule { }
