b367d0b09349552597c722e8f934227c54e2ca7f
Kristina <kristina.sinakovica@inbox.lv>
2020-08-01 11:19:37 +0300

    module stock-items created:

---
+++ src/app/modules/stock-items/components/list-stock-items/list-stock-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-stock-items',
+  templateUrl: './list-stock-items.component.html',
+  styleUrls: ['./list-stock-items.component.css']
+})
+export class ListStockItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
