94f4348792e4b82ad956d4e7753aa477054c9355
Kristina <kristina.sinakovica@inbox.lv>
2020-08-05 23:13:13 +0300

    article form creation

--- src/app/modules/articles/components/create-article/create-article.component.ts
+++ src/app/modules/articles/components/create-article/create-article.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ArticlesService} from "../../services/articles.service";
+import {AuthService} from "../../../shared/auth.service";
+import {EnumsService} from "../../../shared/enums.service";
+
 
 @Component({
   selector: 'app-create-article',
@@ -6,10 +12,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-article.component.css']
 })
 export class CreateArticleComponent implements OnInit {
+    edition = [];
+    rarity = [];
+    cardType = [];
 
-  constructor() { }
+    articleForm = new FormGroup({
+    boosterSet: new FormControl(''),
+    cardName: new FormControl(''),
+    edition: new FormControl(''),
+    rarity: new FormControl(''),
+    cardType: new FormControl(''),
+  });
+
+  constructor(
+    private articlesService: ArticlesService,
+    private router: Router,
+    private enumsService: EnumsService,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.getEnums();
+  }
+
+  getEnums() {
+    this.enumsService.getCardTypes().subscribe(
+      res => this.cardType = res
+    );
+    this.enumsService.getCardRarities().subscribe(
+      res => this.rarity = res
+    );
+    this.enumsService.getCardEditions().subscribe(
+      res => this.edition = res
+    );
+  }
+  saveArticle() {
+    console.log(this.articleForm.getRawValue());
+    this.articlesService.saveArticle(this.articleForm.getRawValue()).subscribe();
+
   }
 
 }
--- src/app/modules/articles/services/articles.service.ts
+++ src/app/modules/articles/services/articles.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {SecureHttpClientService} from "../../shared/secure-http-client.service";
+import {Observable} from "rxjs";
+import {Article} from "../entities/article";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
 
-  constructor() { }
+
+  constructor(private secureHttpClientService: SecureHttpClientService) {
+}
+
+  getArticles(): Observable<any> {
+  return this.secureHttpClientService.get('http://localhost:8080/article/get/id/1');
+}
+
+  saveArticle(article: Article): Observable<any> {
+  return this.secureHttpClientService.post('http://localhost:8080/article/save', article);
+}
+
+  updateArticle(article: Article): Observable<any> {
+  return this.secureHttpClientService.post('http://localhost:8080/article/update', article);
+}
+
+  getAllArticles(): Observable<any> {
+  return this.secureHttpClientService.get('http://localhost:8080/article/all');
+}
+
+  deleteArticle(article: Article): Observable<any> {
+  return this.secureHttpClientService.post('http://localhost:8080/article/delete', article);
+}
 }
