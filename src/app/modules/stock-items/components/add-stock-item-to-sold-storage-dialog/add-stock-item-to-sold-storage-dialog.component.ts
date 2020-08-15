292a975eaaf0c19d98b4b07e1c5f5be68055c943
Vineta <vineta.gajevska@gmail.com>
2020-08-15 16:06:58 +0300

    "ADD TO SOLD" button functional with dialog.

--- src/app/modules/stock-items/components/add-stock-item-to-sold-storage-dialog/add-stock-item-to-sold-storage-dialog.component.ts
+++ src/app/modules/stock-items/components/add-stock-item-to-sold-storage-dialog/add-stock-item-to-sold-storage-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {StockItem} from "../../entities/stock-item";
+import {StockItemsService} from "../../services/stock-items.service";
+import {conditionallyCreateMapObjectLiteral} from "@angular/compiler/src/render3/view/util";
 
 @Component({
   selector: 'app-add-stock-item-to-sold-storage-dialog',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-stock-item-to-sold-storage-dialog.component.css']
 })
 export class AddStockItemToSoldStorageDialogComponent implements OnInit {
+  private stockItemsService: StockItemsService;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public dialogRef: MatDialogRef<AddStockItemToSoldStorageDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public stockItem: StockItem,
+  ) {
   }
 
+  ngOnInit(): void {
+    console.log(this.stockItem);
+  }
+
+  //
+  // saveStockItemAsSold() {
+  //   this.stockItemsService.saveStockItemAsSold(this.stockItem).subscribe(
+  //     res => this.dialogRef.close()
+  //   );
+  // }
+
+  saveStockItem() {
+    this.stockItemsService.updateStockItem(this.stockItem).subscribe(
+      res => this.dialogRef.close()
+    );
+  }
+  close(): void {
+    this.dialogRef.close();
+  }
 }
