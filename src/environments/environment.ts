bfc9369354469e5876ecbb04be942218598bc921
Gatis <gatis.innus@gmail.com>
2020-08-22 15:40:20 +0300

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cryptic-island-35886.herokuapp.com'
+  apiUrl: 'https://cryptic-island-35886.herokuapp.com',
+  frontUrl: 'https://yugioh-front.herokuapp.com',
 };
